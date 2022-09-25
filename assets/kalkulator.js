5317d40d12ef856a1d89ead30b8fa9cfa124e8b3
Agung Prasetyo <93957986+agpprastyo@users.noreply.github.com>
2022-09-25 20:35:46 +0700

    update calculator

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,1 +1,59 @@
 console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+  };
+   
+  function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+  }
+   
+  function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+  }
+   
+  function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+  }
+   
+  const buttons = document.querySelectorAll('.button');
+for (const button of buttons) {
+  button.addEventListener('click', function (event) {
+    // mendapatkan objek elemen yang diklik
+    const target = event.target;
+ 
+    if (target.classList.contains('clear')) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+ 
+    if (target.classList.contains('negative')) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains('equals')) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains('operator')) {
+      handleOperator(target.innerText);
+      return;
+    }
+ 
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
+}
